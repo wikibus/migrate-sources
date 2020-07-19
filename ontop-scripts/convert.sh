1041248f1e28cf82c789f5982cf40a17dc449e32
tpluscode <tpluscode@users.noreply.github.com>
2020-07-19 14:40:33 +0200

    always download driver

--- ontop-scripts/convert.sh
+++ ontop-scripts/convert.sh
@@ -12,9 +12,10 @@ else
   wget https://github.com/ontop/ontop/releases/download/ontop-4.0.0-rc-1/ontop-cli-4.0.0-rc-1.zip
   unzip ontop-cli-4.0.0-rc-1.zip
   cd ..
-  # download sql driver
-  wget https://github.com/microsoft/mssql-jdbc/releases/download/v8.2.2/mssql-jdbc-8.2.2.jre13.jar -P ontop/jdbc/
 fi
+
+# download sql driver
+wget https://github.com/microsoft/mssql-jdbc/releases/download/v8.2.2/mssql-jdbc-8.2.2.jre13.jar -P ontop/jdbc/
 
 touch triples.nt
 BEFORE=$(wc -l triples.nt)
