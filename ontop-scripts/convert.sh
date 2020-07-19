7b0b314fed59066e852de6082bf44cbdb8c4f3fc
tpluscode <tpluscode@users.noreply.github.com>
2020-07-19 15:22:55 +0200

    stream turtle output

--- ontop-scripts/convert.sh
+++ ontop-scripts/convert.sh
@@ -23,7 +23,7 @@ echo "jdbc.password=$SQL_PASSWORD" >> sql.properties
 
 touch triples.nt
 BEFORE=$(wc -l triples.nt)
-./ontop/ontop materialize -m ../src-gen/mapping.r2rml.ttl -f ntriples -o ./triples.nt -p sql.properties
+./ontop/ontop materialize -m ../src-gen/mapping.r2rml.ttl -f turtle -p sql.properties
 
 # show triples count before/after
 AFTER=$(wc -l triples.nt)
